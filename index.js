18cf252072222b16edba481e5fc23d6a45c880a3
SpaghettiNoire <spaghetti@region13.tk>
2019-11-20 07:51:14 +0000

    Added scoping tool

---
+++ index.js
@@ -0,0 +1,33 @@
+////////////////////////////////////////////////////////////////////////////////
+
+/* An object with no prototype works fine as a string-value map even if
+  someone messed with `Object.prototype` (a BAd idea).
+*/
+const nullObject = object => Object.assign(Object.create (null), object);
+
+module.exports = (defaults) => {
+  const scope = nullObject(defaults);
+
+  const backtrackInstructions = [];
+  return {
+    push (newProps) {
+      const backtracking = nullObject();
+      Object.entries(newProps).forEach(([key, value]) => {
+
+        const oldValue = scope[key];
+        if (value !== oldValue) {
+          backtracking[key] = oldValue;
+          scope[key] = value;
+        }
+      });
+      backtrackInstructions.push(backtracking);
+    },
+    pop (times = 1) {
+      let i = times;
+      while (i --> 0) {
+        Object.assign (scope, backtrackInstructions.pop());
+      }
+    },
+    scope
+  }
+};
